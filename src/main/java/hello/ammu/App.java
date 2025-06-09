3a8e5c158d5250ba1d56961f269bd4b6d28503f5
Chaithanya <gpchaithanya25@gmail.com>
2025-06-09 12:18:07 +0530

    Update App.java

--- src/main/java/hello/ammu/App.java
+++ src/main/java/hello/ammu/App.java
@@ -1,10 +1,19 @@
 package hello.ammu;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        // Get current server time
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("Server time: " + now.format(formatter));
     }
 }
